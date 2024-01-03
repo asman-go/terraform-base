a5664ff60a6f83025f636bf473ae3a23788354fd
Oleg Petrakov <petrakov.oleg@yandex.ru>
2024-01-03 12:02:04 +0700

    Skip s3 checksum validations and metadata checks

--- deploy/terraform.tf
+++ deploy/terraform.tf
@@ -1,11 +1,14 @@
 terraform {
   backend "s3" {
     # endpoint = "storage.yandexcloud.net"
-    key                         = "asman-remote-state"
+    key                         = "asman.tfstate"
     dynamodb_table              = "lock"
+
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_metadata_api_check     = true
     skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 
   required_providers {
